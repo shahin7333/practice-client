1a75bc9ad23724435d851a51bf287fcd4d6f7773
shahin7333 <shaparan7333@gmail.com>
2024-03-21 00:50:52 +0600

    update

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -3,21 +3,22 @@ import ProductList from "../../components/client/common/ProductList";
 import axiosInstance from "../../services/axiosInstance";
 
 const page = async () => {
-  const products = await axiosInstance.get("/products?page=1&limit=8");
-  const allProducts = products.data.payload.products;
+  const products = await axiosInstance.get("/categories");
+  const allCategory = products.data.payload.categories;
   return (
     <>
-      <ProductList
-        data={allProducts}
-        title="Latest products"
-        total={allProducts.length}
-        des="Highlighting its main purpose and key features"
-      />
-      <ProductList
+      {allCategory?.map((category) => (
+        <ProductList
+          id={category._id}
+          title={category?.category_name}
+          des="Highlighting its main purpose and key features"
+        />
+      ))}
+      {/* <ProductList
         data={allProducts}
         title="Similar products"
         des="Highlighting its main purpose and key features"
-      />
+      /> */}
     </>
   );
 };
--- components/client/common/ProductList.jsx
+++ components/client/common/ProductList.jsx
@@ -1,34 +1,40 @@
-"use client";
 import React from "react";
 import SingleProduct from "./SingleProduct";
 import Link from "next/link";
+import axiosInstance from "../../../services/axiosInstance";
 
-const ProductList = ({ data, title, des, total = false }) => {
-  console.log("data", data);
+const ProductList = async ({ id, title, des }) => {
+  const products = await axiosInstance.get(
+    `/products?page=1&limit=8&categoryId=${id}`
+  );
+  const allProducts = products.data.payload.products;
+  console.log(allProducts);
+
   return (
-    <div className="mx-auto max-w-[1440px] overflow-hidden my-8  px-4 sm:px-6 lg:px-8 xl:px-20">
-      <div className="mb-8">
-        <h2 className="text-2xl md:text-4xl font-semibold">{title}</h2>
-        <p className="text-gray-700">{des}</p>
-        {total && (
-          <p className="mt-6 text-sm text-end">
-            {total} items found for{" "}
-            <span className="text-indigo-500 font-medium">"{title}"</span>
-          </p>
-        )}
-      </div>
+    <>
+      {allProducts.length > 0 && (
+        <div className="mx-auto max-w-[1440px] overflow-hidden my-8  px-4 sm:px-6 lg:px-8 xl:px-20">
+          <div className="mb-8">
+            <h2 className="text-2xl md:text-4xl font-semibold">{title}</h2>
+            <p className="text-gray-700">{des}</p>
+          </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {data?.map((item) => (
-          <SingleProduct key={item.id} product={item} />
-        ))}
-      </div>
-      <div className="pt-8 flex justify-end">
-        <Link href="/products" className="px-6 py-2 bg-indigo-500 text-white">
-          View All
-        </Link>
-      </div>
-    </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {allProducts?.map((item) => (
+              <SingleProduct key={item.id} product={item} />
+            ))}
+          </div>
+          <div className="pt-8 flex justify-end">
+            <Link
+              href="/products"
+              className="px-6 py-2 bg-indigo-500 text-white"
+            >
+              View All
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
