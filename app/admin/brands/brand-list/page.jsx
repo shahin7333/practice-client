3a4ee977cabaf4602e1018d023f73381645d4d37
shahin7333 <shaparan7333@gmail.com>
2024-03-22 17:30:02 +0600

    update

--- app/admin/brands/brand-list/page.jsx
+++ app/admin/brands/brand-list/page.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import Brand from "../../../../components/admin/brands/Brand"
+import Brand from "../../../../components/admin/brands/Brand";
+import axiosInstance from "../../../../services/axiosInstance";
 
-const page = () => {
-  return <Brand />;
+const page = async () => {
+  const brandsList = await axiosInstance.get("/brands");
+  return <Brand brands={brandsList.data.payload.brands} />;
 };
 
 export default page;
