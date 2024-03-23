391f99f7b91b5ae19f2b958a06425a4a12ea8d60
Md.Saifur Rahman Rasel <saifurrahmanrasel4627@gmail.com>
2024-03-23 23:15:34 +0600

    update

--- components/client/cart/SCart.jsx
+++ components/client/cart/SCart.jsx
@@ -1,196 +1,152 @@
-"use client";
-import {
-  CheckIcon,
-  ClockIcon,
-  QuestionMarkCircleIcon,
-  XMarkIcon as XMarkIconMini,
-} from "@heroicons/react/20/solid";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+'use client'
 
-const products = [
-  {
-    id: 1,
-    name: "Basic Tee",
-    href: "#",
-    price: "$32.00",
-    color: "Sienna",
-    inStock: true,
-    size: "Large",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in sienna.",
-  },
-  {
-    id: 2,
-    name: "Basic Tee",
-    href: "#",
-    price: "$32.00",
-    color: "Black",
-    inStock: false,
-    leadTime: "3–4 weeks",
-    size: "Large",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-02.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-  },
-  {
-    id: 3,
-    name: "Nomad Tumbler",
-    href: "#",
-    price: "$35.00",
-    color: "White",
-    inStock: true,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-03.jpg",
-    imageAlt: "Insulated bottle with white base and black snap lid.",
-  },
-];
+import Link from 'next/link'
+import toast from 'react-hot-toast'
+import { useEffect, useState } from 'react'
+import axiosInstance from '../../../services/axiosInstance'
+import { XMarkIcon as XMarkIconMini } from '@heroicons/react/20/solid'
 
-const SCart = () => {
-  const router = useRouter();
+const SCart = ({ cartData }) => {
+  const [carts, setCarts] = useState(cartData)
+  let subtotalPrice = 0
+
+  useEffect(() => {
+    setCarts(cartData)
+  }, [cartData])
+
+  const handleDeleteCart = id => {
+    axiosInstance.delete(`/cart/${id}`).then(res => {
+      toast.success(res.data.message)
+      setCarts(res.data.payload.carts)
+    })
+  }
+
+  const handleUpdateQty = (value, id) => {
+    axiosInstance.patch(`/cart/${id}`, { quantity: value,customerId: }).then(res => {
+      toast.success(res.data.message)
+      setCarts(res.data.payload.carts)
+    })
+  }
+
+  carts.forEach(cart => {
+    subtotalPrice += cart.totalPrice
+  })
+
   return (
-    <div className="mx-auto px-4 pb-24 pt-16 sm:px-6 max-w-[1440px] lg:px-20">
-      <h1 className="text-xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+    <div className='mx-auto px-4 pb-24 pt-16 sm:px-6 max-w-[1440px] lg:px-20'>
+      <h1 className='text-xl font-bold tracking-tight text-gray-900 sm:text-3xl'>
         Shopping Cart
       </h1>
-      <p className="text-sm mt-2">Items in your shopping cart</p>
-      <form className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
-        <section aria-labelledby="cart-heading" className="lg:col-span-7">
+      <p className='text-sm mt-2'>Items in your shopping cart</p>
+      <form className='mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16'>
+        <section aria-labelledby='cart-heading' className='lg:col-span-7'>
           <ul
-            role="list"
-            className="divide-y divide-gray-200 border-b border-t border-gray-200"
+            role='list'
+            className='divide-y divide-gray-200 border-t border-gray-200'
           >
-            {products.map((product, productIdx) => (
-              <li key={product.id} className="flex py-4">
-                <div className="flex-shrink-0">
-                  <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="h-24 w-24 object-cover object-center"
-                  />
-                </div>
-
-                <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
-                  <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+            {carts?.length > 0 ? (
+              carts.map(item => (
+                <li
+                  key={item._id}
+                  className='grid grid-cols-12 items-center py-4'
+                >
+                  <div className='col-span-6 flex items-start gap-4'>
+                    <img
+                      src={'/assets/default-image.jpg'}
+                      alt={item.product.product_name}
+                      className='h-24 w-24 object-cover object-center'
+                    />
                     <div>
-                      <div className="flex justify-between">
-                        <h3 className="text-sm">
+                      <div className='flex justify-between'>
+                        <h3 className='text-sm'>
                           <Link
-                            href={product.href}
-                            className="font-medium text-gray-700 hover:text-gray-800"
+                            href=''
+                            className='font-medium text-gray-700 hover:text-gray-800'
                           >
-                            {product.name}
+                            {item.product.product_name}
                           </Link>
                         </h3>
                       </div>
-                      <div className="mt-1 flex text-xs">
-                        <p className="text-gray-500">{product.color}</p>
-                        {product.size ? (
-                          <p className="ml-4 border-l border-gray-200 pl-4 text-gray-500">
-                            {product.size}
-                          </p>
-                        ) : null}
-                      </div>
-                      <p className="mt-1 text-sm font-medium text-indigo-600">
-                        {product.price}
+                      <p className='mt-1 text-sm font-medium text-indigo-600'>
+                        {item.product.price}
                       </p>
-                    </div>
-
-                    <div className="mt-4 sm:mt-0 sm:pr-9">
-                      <select
-                        id={`quantity-${productIdx}`}
-                        name={`quantity-${productIdx}`}
-                        className="max-w-full rounded-md border border-gray-300 py-1.5 text-left text-base font-medium leading-5 text-gray-700 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
-                      >
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                        <option value={3}>3</option>
-                        <option value={4}>4</option>
-                        <option value={5}>5</option>
-                        <option value={6}>6</option>
-                        <option value={7}>7</option>
-                        <option value={8}>8</option>
-                      </select>
-
-                      <div className="absolute right-0 top-0">
-                        <button
-                          type="button"
-                          className="-m-2 inline-flex p-2 text-gray-400 hover:text-gray-500"
-                        >
-                          <span className="sr-only">Remove</span>
-                          <XMarkIconMini
-                            className="h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        </button>
-                      </div>
                     </div>
                   </div>
 
-                  <p className="mt-3 flex space-x-2 text-xs text-gray-700">
-                    {product.inStock ? (
-                      <CheckIcon
-                        className="h-4 w-4 flex-shrink-0 text-green-500"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <ClockIcon
-                        className="h-4 w-4 flex-shrink-0 text-gray-300"
-                        aria-hidden="true"
-                      />
-                    )}
+                  <div className='col-span-6 flex justify-between items-center gap-5'>
+                    <select
+                      value={item.quantity}
+                      onChange={e => handleUpdateQty(e.target.value, item._id)}
+                      className='rounded-md border border-gray-300 py-1.5 text-left text-base font-medium leading-5 text-gray-700 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm'
+                    >
+                      <option value={1}>1</option>
+                      <option value={2}>2</option>
+                      <option value={3}>3</option>
+                      <option value={4}>4</option>
+                      <option value={5}>5</option>
+                      <option value={6}>6</option>
+                      <option value={7}>7</option>
+                      <option value={8}>8</option>
+                    </select>
 
-                    <span>
-                      {product.inStock
-                        ? "In stock"
-                        : `Ships in ${product.leadTime}`}
-                    </span>
-                  </p>
-                </div>
-              </li>
-            ))}
+                    <p>{item.totalPrice}</p>
+
+                    <button
+                      type='button'
+                      className='-m-2 inline-flex p-2 text-red-600'
+                      onClick={() => handleDeleteCart(item._id)}
+                    >
+                      <span className='sr-only'>Remove</span>
+                      <XMarkIconMini className='h-5 w-5' aria-hidden='true' />
+                    </button>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <p className='py-24 text-center'>There is no cart items</p>
+            )}
           </ul>
         </section>
 
-        {/* Order summary */}
         <section
-          aria-labelledby="summary-heading"
-          className="mt-16 bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
+          aria-labelledby='summary-heading'
+          className='mt-16 bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8'
         >
           <h2
-            id="summary-heading"
-            className="text-lg font-medium text-gray-900"
+            id='summary-heading'
+            className='text-lg font-medium text-gray-900'
           >
             Order summary
           </h2>
 
-          <dl className="mt-6 space-y-4">
-            <div className="flex items-center justify-between">
-              <dt className="text-sm text-gray-600">Subtotal</dt>
-              <dd className="text-sm font-medium text-gray-900">$99.00</dd>
+          <dl className='mt-6 space-y-4'>
+            <div className='flex items-center justify-between'>
+              <dt className='text-sm text-gray-600'>Subtotal</dt>
+              <dd className='text-sm font-medium text-gray-900'>
+                {subtotalPrice}
+              </dd>
             </div>
-            <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-              <dt className="flex text-sm text-gray-600">
+            <div className='flex items-center justify-between border-t border-gray-200 pt-4'>
+              <dt className='flex text-sm text-gray-600'>
                 <span>Tax estimate</span>
-                <p className="ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500">
-                  <span className="text-xs">20%</span>
+                <p className='ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500'>
+                  <span className='text-xs'>20%</span>
                 </p>
               </dt>
-              <dd className="text-sm font-medium text-gray-900">$8.32</dd>
+              <dd className='text-sm font-medium text-gray-900'>{subtotalPrice * 0.2}</dd>
             </div>
-            <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-              <dt className="text-base font-medium text-gray-900">
+            <div className='flex items-center justify-between border-t border-gray-200 pt-4'>
+              <dt className='text-base font-medium text-gray-900'>
                 Order total
               </dt>
-              <dd className="text-base font-medium text-gray-900">$112.32</dd>
+              <dd className='text-base font-medium text-gray-900'>{subtotalPrice * 1.2}</dd>
             </div>
           </dl>
 
-          <div className="mt-6">
+          <div className='mt-6'>
             <button
-              type="submit"
-              className="w-full border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+              type='submit'
+              className='w-full border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50'
             >
               Checkout
             </button>
@@ -198,7 +154,7 @@ const SCart = () => {
         </section>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default SCart;
+export default SCart
--- components/client/product-details/ProductDetails.jsx
+++ components/client/product-details/ProductDetails.jsx
@@ -1,135 +1,113 @@
-"use client";
-import { Disclosure, Tab } from "@headlessui/react";
-import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
-import { useAtom } from "jotai";
-import { countAtom } from "../../../atom";
-import Link from "next/link";
+'use client'
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+import Image from 'next/image'
+// import { useAtom } from 'jotai'
+import toast from 'react-hot-toast'
+// import { countAtom } from '../../../atom'
+import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import { Disclosure } from '@headlessui/react'
+import axiosInstance from '../../../services/axiosInstance'
+import { MinusIcon, PlusIcon } from '@heroicons/react/24/outline'
+
+function classNames (...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
 const ProductDetails = ({ data }) => {
-  console.log("details", data);
-  const [count, setCount] = useAtom(countAtom);
+  const router = useRouter()
+  const session = useSession()
+  // const [count, setCount] = useAtom(countAtom)
+
+  console.log(session?.data?.user)
+
   const addtoCart = () => {
-    setCount(count + 1);
-  };
+    session.status === 'authenticated' &&
+    session?.data?.user?.role === 'customer'
+      ? axiosInstance
+          .post('/cart', {
+            productId: data._id,
+            customerId: session?.data?.user._id,
+            quantity: 1
+          })
+          .then(res => {
+            toast.success(res.data.message)
+            router.push('/cart')
+          })
+      : toast.error('Please login first')
+    // setCount(count + 1);
+  }
+
   return (
     <>
-      <div className="mx-auto p-4 sm:p-6 lg:p-8 xl:p-20 lg:max-w-[1440px]">
-        <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-          {/* <Tab.Group as="div" className="flex flex-col-reverse">
-            <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
-              <Tab.List className="grid grid-cols-4 gap-6">
-                {data?.images?.map((image) => (
-                  <Tab
-                    key={image.id}
-                    className="relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
-                  >
-                    {({ selected }) => (
-                      <>
-                        <span className="absolute inset-0 overflow-hidden rounded-md">
-                          <img
-                            src={data?.image || "/assets/default-image.jpg"}
-                            alt=""
-                            className="h-full w-full object-cover object-center"
-                          />
-                        </span>
-                        <span
-                          className={classNames(
-                            selected ? "ring-indigo-500" : "ring-transparent",
-                            "pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2"
-                          )}
-                          aria-hidden="true"
-                        />
-                      </>
-                    )}
-                  </Tab>
-                ))}
-              </Tab.List>
-            </div>
-
-            <Tab.Panels className="aspect-h-1 aspect-w-1 w-full">
-              {data?.images?.map((image) => (
-                <Tab.Panel key={image.id}>
-                  <img
-                    src={image.src}
-                    alt={image.alt}
-                    className="h-full w-full object-cover object-center sm:rounded-lg"
-                  />
-                </Tab.Panel>
-              ))}
-            </Tab.Panels>
-          </Tab.Group> */}
+      <div className='mx-auto p-4 sm:p-6 lg:p-8 xl:p-20 lg:max-w-[1440px]'>
+        <div className='lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8'>
           <Image
-            src={data?.image || "/assets/default-image.jpg"}
-            alt=""
-            className="max-h-[500px] w-full object-cover object-center"
-            height="550"
-            width="700"
+            src={'/assets/default-image.jpg'}
+            alt=''
+            className='max-h-[500px] w-full object-cover object-center'
+            height='550'
+            width='700'
           />
 
-          <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-            <h1 className="text-2xl font-semibold tracking-tight text-gray-900">
+          <div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>
+            <h1 className='text-2xl font-semibold tracking-tight text-gray-900'>
               {data.product_name}
             </h1>
 
-            <div className="mt-3">
-              <p className="text-lg font-medium tracking-tight text-indigo-600">
+            <div className='mt-3'>
+              <p className='text-lg font-medium tracking-tight text-indigo-600'>
                 ${data.price}
               </p>
             </div>
 
-            <div className="mt-4">
-              <p className="text-xs text-gray-700">{data.description}</p>
+            <div className='mt-4'>
+              <p className='text-xs text-gray-700'>{data.description}</p>
             </div>
-            <div className="mt-10">
-              <Link
-                href="/cart"
+            <div className='mt-10'>
+              <button
                 onClick={addtoCart}
-                className=" bg-indigo-600 px-8 py-2 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                className=' bg-indigo-600 px-8 py-2 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50'
               >
                 Add to cart
-              </Link>
+              </button>
             </div>
 
-            <section aria-labelledby="details-heading" className="mt-12">
-              <div className="divide-y divide-gray-200 border-t">
-                <Disclosure as="div">
+            <section aria-labelledby='details-heading' className='mt-12'>
+              <div className='divide-y divide-gray-200 border-t'>
+                <Disclosure as='div'>
                   {({ open }) => (
                     <>
                       <h3>
-                        <Disclosure.Button className="group relative flex w-full items-center justify-between py-6 text-left">
+                        <Disclosure.Button className='group relative flex w-full items-center justify-between py-6 text-left'>
                           <span
                             className={classNames(
-                              open ? "text-indigo-600" : "text-gray-900",
-                              "text-sm font-medium"
+                              open ? 'text-indigo-600' : 'text-gray-900',
+                              'text-sm font-medium'
                             )}
                           >
                             Additional details
                           </span>
-                          <span className="ml-6 flex items-center">
+                          <span className='ml-6 flex items-center'>
                             {open ? (
                               <MinusIcon
-                                className="block h-6 w-6 text-indigo-400 group-hover:text-indigo-500"
-                                aria-hidden="true"
+                                className='block h-6 w-6 text-indigo-400 group-hover:text-indigo-500'
+                                aria-hidden='true'
                               />
                             ) : (
                               <PlusIcon
-                                className="block h-6 w-6 text-gray-400 group-hover:text-gray-500"
-                                aria-hidden="true"
+                                className='block h-6 w-6 text-gray-400 group-hover:text-gray-500'
+                                aria-hidden='true'
                               />
                             )}
                           </span>
                         </Disclosure.Button>
                       </h3>
                       <Disclosure.Panel
-                        as="div"
-                        className="prose prose-sm pb-6"
+                        as='div'
+                        className='prose prose-sm pb-6'
                       >
-                        <ul role="list" className="flex flex-col gap-2 text-sm">
+                        <ul role='list' className='flex flex-col gap-2 text-sm'>
                           <li>Quantity: {data?.quantity}</li>
                           <li>Stock In: {data?.stock}</li>
                           <li>Brand Name: {data?.brand?.brand_name}</li>
@@ -143,13 +121,8 @@ const ProductDetails = ({ data }) => {
           </div>
         </div>
       </div>
-      {/* <ProductList
-        data={data}
-        title="Similar products"
-        des="Highlighting its main purpose and key features"
-      /> */}
     </>
-  );
-};
+  )
+}
 
-export default ProductDetails;
+export default ProductDetails
